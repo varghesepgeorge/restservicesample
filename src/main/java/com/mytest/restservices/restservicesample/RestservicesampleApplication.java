921c222c37a0d786e72f22a74f8ae9e157de8f63
Tiby George <TGP@Macintosh.local>
2019-08-30 16:02:11 +0530

    Initial commit

---
+++ src/main/java/com/mytest/restservices/restservicesample/RestservicesampleApplication.java
@@ -0,0 +1,13 @@
+package com.mytest.restservices.restservicesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicesampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicesampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytest/restservices/restservicesample/RestservicesampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytest.restservices.restservicesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestservicesampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
